f1c30cc30e08e36028c309a9b0bd37addfc77aee
tienak1 <thattien1111@gmail.com>
2021-11-30 14:05:54 +0000

    a

--- models/index.js
+++ models/index.js
@@ -1,10 +1,13 @@
 const Sequelize = require('sequelize');
 
 const initModels = require("./init-models");
-
-const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
+console.log("Name",process.env.DB_NAME);
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`,
     dialect: 'mysql',
+    dialectOptions: {
+        socketPath: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`
+    },
 });
 
 module.exports = {
